5f334b794ff68b6efc698bc82d3b0b8f28dfdb7a
FMradio87 <52276030+FMradio87@users.noreply.github.com>
2019-07-04 02:36:02 -0700

    変更
    もう何をどうすれば良いのかわからない

--- nlp_27.py
+++ nlp_27.py
@@ -8,8 +8,8 @@ import nlp_26
 
 #リンクマークアップは[[,|,]]だと思うからそれをsubで置換する
 
-refarence_data2 = nlp_26
+refarence_data2 = nlp_26.retult3
+for i in refarence_data2:
+    link_mark = re.sub(r'[\|]',(''),i)
+    print(link_mark)
 
-link_mark = re.sub(r'[\[\[],[\|],[\]\]]',(''),refarence_data2)
-
-print(link_mark)
