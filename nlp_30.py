4604f0efcae99137c3833be5c2362eced9588737
FMradio87 <52276030+FMradio87@users.noreply.github.com>
2019-10-03 18:38:12 +0900

    ４章

--- nlp_30.py
+++ nlp_30.py
@@ -8,18 +8,24 @@
 '''
 なんか夏休み前にneko.txt.macabを作ってた。
 これそのまま使えるかなと思ってやろうとしてるけど失敗してます
+できた！？やった！！例外処理を使ったら大丈夫になった！
+（c2でエラーが起きる可能性があったからtryで例外はそのまま続けてねってやった）
+morphme(形態素) mapping(マッピング)
 '''
 
 def base_file():
-    L = []
-    with open('neko.txt.mecab') as twofile:
+    morpheme = []
+    with open('neko.txt.mecab',mode='r+',encoding='utf-8') as twofile:
         for a in twofile:
             c1 = a.split('\t')
-            c2 = c1[1].split(',')
-            maping = {'surfase': c1[0],'base': c2[6],'pos': c2[0],'pos1': c2[1]}
-            L.append(maping)
-        return L
+            try:
+                c2 = c1[1].split(',')
+            except IndexError:
+                continue
+            mapping = {'surfase': c1[0], 'base': c2[6], 'pos': c2[0], 'pos1': c2[1]}
+            morpheme.append(mapping)
+    return morpheme
 
 if __name__ == '__main__':
-    for a in base_file():
-        print(a)
+    for b in base_file():
+        print(b)
